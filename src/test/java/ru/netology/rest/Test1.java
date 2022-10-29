6455578c66c8a965759a0c5fa24b5a8689dd762c
NatalyaZinger <nata.zinger@gmail.com>
2022-10-29 14:58:28 +0500

    final

--- src/test/java/ru/netology/rest/Test1.java
+++ src/test/java/ru/netology/rest/Test1.java
@@ -18,6 +18,6 @@ class Test1 {
 // Проверки
                 .then()
                 .statusCode(200)
-                .body("data", equalTo("some value"));
+                .body("data", equalTo("some data"));
     }
 }
